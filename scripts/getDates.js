d60ea83c331ebd23c5676e52b22b698cb25dcfd6
seantate2005 <161382219+seantate2005@users.noreply.github.com>
2024-04-29 12:01:03 -0400

    k

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -1,2 +1,12 @@
-document.getElementById('year').innertext = new Date().getFullYear();
-document.getElementById('lastmodified').innerText = "Last modified: " + document.lastModified; 
+function getCurrentYear() {
+    return new Date().getFullYear();
+}
+
+function getLastModifiedDate() {
+    return document.lastModified;
+}
+
+document.getElementById("year").innerText = getCurrentYear();
+
+
+document.getElementById("lastModified").innerText = "Last Modified: " + getLastModifiedDate();
