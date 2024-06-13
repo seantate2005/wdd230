f1c6e0c91816fc054ebd6b760fd25c3105e7e246
seantate2005 <161382219+seantate2005@users.noreply.github.com>
2024-06-13 12:56:01 -0400

    d

---
+++ week02/scripts/bom.js
@@ -0,0 +1,4 @@
+const inoput = document.querySelector('#favchap'); 
+const button = document.querySelector('button');
+const list = document.querySelector('#list');
+button.addEventListener('click', function () {}
