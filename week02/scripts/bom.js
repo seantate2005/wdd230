4226a81fc51c97c54d43ad77c34e5837eac3d495
seantate2005 <161382219+seantate2005@users.noreply.github.com>
2024-06-20 17:06:01 -0400

    2

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,4 +1,15 @@
 const input = document.querySelector('#favchap'); 
 const button = document.querySelector('button');
 const list = document.querySelector('#list');
-button.addEventListener('click', function ('Scripture has been added.)');
+button.addEventListener('click', function ());
+const li = document.createElement(`li`);
+const deleteButton = document.createElement(`button`);
+li.textContent= input.ariaValueMax; 
+deleteButton.textContent = `✖️`; 
+li.append(deleteButton);
+list.append(li);
+deleteButton.addEventListener(`click`, function ()(
+    list.removeChild(li);
+    input.focus();
+));
+input.value=``;
