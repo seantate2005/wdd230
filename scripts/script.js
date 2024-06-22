1f90e51f5f1308bbf264c8d9fa240f6fb17d901a
seantate2005 <161382219+seantate2005@users.noreply.github.com>
2024-06-22 12:58:44 -0400

    d

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,23 @@
 const modeButton = document.querySelector("#mode");
-const main = document.querySelector("main");
+const main = document.querySelector("body");
 
 modeButton.addEventListener("click", () => {
 	if (modeButton.textContent.includes("🌛")) {
-		main.style.background = "#000";
-		main.style.color = "#fff";
+		main.style.background = "black";
+		main.style.color = "orange"
 		modeButton.textContent = "🌞";
 	} else {
 		main.style.background = "#eee";
-		main.style.color = "#000";
+		main.style.color = "navy";
 		modeButton.textContent = "🌛";
 	}
 });
+
+function myFunction() {
+	var x = document.getElementById("myTopnav");
+	if (x.className === "topnav") {
+	  x.className += " responsive";
+	} else {
+	  x.className = "topnav";
+	}
+  }
